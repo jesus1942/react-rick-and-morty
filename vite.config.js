0ef7da70a4583be997f871696d5b7b714b7b9d4f
jesus1942 <chucky9425@gmail.com>
2023-07-10 10:10:12 -0300

    cambio en el archivo de vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://jesus1942.github.io/rickAndMortyApp/",
+  base: "https://jesus1942.github.io/react-rick-and-morty/",
   plugins: [react()],
 })
